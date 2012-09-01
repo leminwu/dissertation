775557814c32c39ce3e20ae58fb7c205d744e68f
Lemin Wu <lemin.t.wu@gmail.com>
2012-09-01 14:08:55 +0100

    Add premium calculation code

---
+++ programs/small_a.cc
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <boost/random.hpp>
+#include "small_a.h"
+
+float reserve(float age, float premium, float A, float a) {
+	return 100000.0f * A - (1.0f-0.01f)*premium*a;
+}
+
+
+int main(int argc, char** argv) {
+	
+    const float A = 0.0f;
+    const float B = 0.0000027f;
+    const float c = 1.124f;
+
+
+    const char* files[2][2] = {
+        {"female_non-smokers_two_years_select", "female_smokers_two_years_select"}, // female
+        {"male_non-smokers_two_years_select", "male_smokers_two_years_select"}, // male
+    };
+
+    std::vector<float> t[2][2][3]; // gender, smoker, duration
+
+    for(int j=0; j<2; j++) {
+        for(int k=0; k<2; k++) {
+            std::cout << "Reading " << files[j][k] << "\n";
+            t[j][k][0].resize(130);
+            t[j][k][1].resize(130);
+            t[j][k][2].resize(130);
+            std::ifstream f(files[j][k]);
+            std::string line;
+            std::getline(f, line); // file header
+            while(!f.eof()) {
+                int age;
+                float duration0, duration1, duration2plus;
+                f >> age >> duration0 >> duration1 >> duration2plus;
+                t[j][k][0][age] = duration0;
+                t[j][k][1][age] = duration1;
+                t[j][k][2][age] = duration2plus;
+            }
+            f.close();
+        }
+    }
+
+    std::cout << "Generating people\n";
+    boost::mt19937 generator;
+    boost::uniform_int<> boolean(0,1);
+    boost::uniform_real<> agedistribution(60, 80);
+    boost::variate_generator<boost::mt19937&, boost::uniform_int<> > gender(generator, boolean);
+    boost::variate_generator<boost::mt19937&, boost::uniform_int<> > smoker(generator, boolean);
+    boost::variate_generator<boost::mt19937&, boost::uniform_real<> > age(generator, agedistribution);
+
+    for(int j=0; j<100; j++) {
+        float personage = age();
+        bool persongender = gender();
+        bool personsmoker = smoker();
+//for loop for each year, 
+        float i = 0.05f;
+        float m = 12.0f;
+        float delta = std::log(1+i);
+        float pvuap = PVUnitAnnualPayment(personage, A, B, c, i, t[persongender][personsmoker], 0);
+        float fom = ForceOfMortality(personage, A, B, c);
+        float a = small_a(pvuap, m, delta, fom);
+        float bigA = big_a(a, m, i);
+        
+        float a_reserve = small_a(pvuap, 1.0f, delta, fom);
+        float bigA_reserve = big_a(a_reserve, 1.0f, i);
+        float premium = Premium(pvuap, fom, delta, m, a, bigA);
+        std::cout << personage << ' '
+                << (persongender?'M':'F') << ' '
+                << (personsmoker?'Y':'N') << ' '
+                << premium << ' '
+                << reserve(personage, premium, bigA_reserve, a_reserve)
+                << std::endl;
+        std::cout << "a="<< a_reserve<<  std::endl;
+    }
+}
+
---
+++ programs/small_a.h
@@ -0,0 +1,61 @@
+#include <cmath>
+#include <vector>
+using namespace std;
+
+float SurvivalRateUltimate(int k, int x, float A, float B, float c) {
+	return std::exp((-B / log(c))*pow(c,x)*(pow(c,k)-1));
+}
+
+float SurvivalRateSelect(int k, int x, float A, float B, float c) {
+	return exp(pow(0.9,2-k) * ((1-pow(0.9,k)) * A) / log(0.9) + (pow(c,k) - pow(0.9,k) * B * pow(c,x)) / log(0.9/c));
+}
+
+
+
+float PVUnitAnnualPayment(float age, float A, float B, float c, float i, vector<float> duration[3], int t) {
+	float peoplesAreDead = 130.0f;
+	float sum = 0.0f;
+	float V = 1.0f/(1.0f+i);
+    for (int k=1; k<int(peoplesAreDead-age);k++){
+        sum += pow(V,k)*(1.0f-duration[min(k-1,2)][int(age)+k-1]);
+    }
+    return sum;
+}
+    
+float PVUnitAnnualPayment2(float age, float A, float B, float c, float i, vector<float> duration[3], int t) {
+        float peoplesAreDead = 130.0f;
+        float sum = 0.0f;
+        float V = 1.0f/(1.0f+i);
+
+	for(int k=t; k<std::min(2,int(peoplesAreDead-age)); k++) {
+		sum += std::pow(V, k) *SurvivalRateSelect(k,age,A,B,c) ; 
+	}
+	for(int k=std::max(2,t); k<int(peoplesAreDead-age); k++) {
+		sum += std::pow(V, k) * SurvivalRateUltimate(k, age+2, A, B, c); //wrong, 
+	}
+	return sum;
+}
+
+
+
+float small_a(float pvUnitAnnualPayment, float m, float delta, float forceOfMortality) {
+    return pvUnitAnnualPayment - (m-1)/(2*m) - ((m*m-1)/(12*m*m))*(delta+forceOfMortality);
+}
+
+float big_a(float a, float m, float i) {
+	float V = 1.0f/(1.0f+i);
+    return 1.0f - a*m*(1.0f - pow(V, 1.0f/m));
+}
+
+float ForceOfMortality(float age, float A, float B, float c) {
+	return A + B*pow(c,age);
+}
+
+float Premium(float pvUnitAnnualPayment, float forceOfMortality, float delta, float m, float a, float A) {
+    float S = 100000.0f;
+    float b = 0.01f;
+    float initial=400.0f;
+    return (S*A)/((1.0f - b)*a - 0.4f);
+}
+
+
